37c59bd34d15e16879c2ada316e99557b4aa1dbd
gibany <oh114114@naver.com>
2025-05-19 17:56:49 +0900

    [오인웅] -thymeleaf 연동 예제

---
+++ src/main/java/com/inong/spring/ex/lifecycle/Ex02Controller.java
@@ -0,0 +1,17 @@
+package com.inong.spring.ex.lifecycle;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Ex02Controller {
+	@RequestMapping("/ex02")
+	public String thymeleafResponse(){
+		//html 파일 경로 리턴
+		//src/main/resources/templates 생략
+		//html 생략가능
+		
+		return "lifecycle/ex02";
+	}
+
+}
