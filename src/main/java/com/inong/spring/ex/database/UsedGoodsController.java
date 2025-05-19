674d1c41b234bbd0bde487fb829a2841577a0ee8
gibany <oh114114@naver.com>
2025-05-19 18:49:46 +0900

    [오인웅] - service,mvc 예제 풀이

---
+++ src/main/java/com/inong/spring/ex/database/UsedGoodsController.java
@@ -0,0 +1,15 @@
+package com.inong.spring.ex.database;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class UsedGoodsController {
+	
+	// 중고 게시글 모든 정보를 response에 담는 페이지
+	@RequestMapping("/db/usedgoods/list")
+	public usedGoodsList() {
+		
+		// 중고 게시글 정보 얻어 오기
+	}
+}
--- src/main/java/com/inong/spring/ex/lifecycle/Ex02Controller.java
+++ src/main/java/com/inong/spring/ex/lifecycle/Ex02Controller.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class Ex02Controller {
-	@RequestMapping("/ex02")
+	@RequestMapping("/lifecycle/ex02")
 	public String thymeleafResponse(){
 		//html 파일 경로 리턴
 		//src/main/resources/templates 생략
