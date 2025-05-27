6b2ed935e554ba6b94ca3ce512f2b3c1e525f533
gibany <oh114114@naver.com>
2025-05-27 17:51:00 +0900

    [오인웅] -thymeleaf 문법 중 변수,조건문 예제 풀이 진행

---
+++ src/main/java/com/inong/spring/ex/thymeleaf/ThymeleafController.java
@@ -0,0 +1,16 @@
+package com.inong.spring.ex.thymeleaf;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ThymeleafController {
+	
+	@GetMapping("/thymeleaf/ex01")
+	public String ex01() {
+		return "thymeleaf/ex01";
+		
+	}
+	
+
+}
